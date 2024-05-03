13ed6fd4bfed41b8f61e399d08ee90aa37a79f05
Sudipta Singha <115441575+sudipta1254@users.noreply.github.com>
2024-05-03 22:56:29 +0530

    Update script.js

--- GoogleMeetUImobile/script.js
+++ GoogleMeetUImobile/script.js
@@ -1,6 +1,6 @@
 $('.fa-solid').on('mouseenter', function () {
     let cls = $(this).attr('class')
-    if (!(cls.includes('-video') || cls.includes('-bars'))
+    if (!(cls.includes('-video') || cls.includes('-bars')))
         $(this).removeClass('fa-regular').addClass('fa-solid')
 })
 .on('mouseleave', function () {

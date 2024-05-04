d250c9ad2f36c36b28ba45cc3dbc99056ba5f9a9
Sudipta Singha <singhaashoka03@gmail.com>
2024-05-04 16:28:45 +0530

    Update icons & bug fix

--- GoogleMeetUImobile/script.js
+++ GoogleMeetUImobile/script.js
@@ -1,27 +1,27 @@
-$('.fa').on('mouseenter', function () {
-    let cls = $(this).attr('class')
-    if (!(cls.includes('-video') || cls.includes('-bars')))
-        $(this).removeClass('fa-regular').addClass('fa-solid')
+/* Change background of icons on hover */
+$('.mdi').on('mouseenter', function() {
+    $(this).css('background', '#c4d8f3')
 })
-.on('mouseleave', function () {
-    let cls = $(this).attr('class')
-    if (!(cls.includes('-video') || cls.includes('-bars')))
-        $(this).removeClass('fa-solid').addClass('fa-regular')
-}) 
-
-/* $('.mdi').on('mouseenter', function () {
-    let cls = $(this).text()
-    if (cls === 'chat_bubble_outline')
-        $(this).text('chat_bubble')
-    if (!cls.includes('videocam'))
-        $(this).removeClass('material-icons-outlined').addClass('material-icons')
+.on('mouseleave', function() {
+    $(this).css('background', 'transparent')
 })
-.on('mouseleave', function () {
-    let cls = $(this).text()
-    if (cls === 'chat_bubble')
-        $(this).text('chat_bubble_outline')
-    if (!cls.includes('videocam'))
-        $(this).removeClass('material-icons').addClass('material-icons-outlined')
-}) */
 
-$('main').css('height', innerHeight-55)
+/* Change style of icons on click */
+$('.md-email').click(function() {
+    $(this).removeClass('material-icons-outlined').addClass('material-icons')
+    $('.md-chat').text('chat_bubble_outline')
+    $('.md-video').addClass('material-icons-outlined').removeClass('material-icons')
+})
+$('.md-chat').click(function() {
+    $('.md-email').addClass('material-icons-outlined').removeClass('material-icons')
+    $(this).text('chat_bubble')
+    $('.md-video').addClass('material-icons-outlined').removeClass('material-icons')
+})
+$('.md-video').click(function() {
+    $('.md-email').addClass('material-icons-outlined').removeClass('material-icons')
+    $('.md-chat').text('chat_bubble_outline')
+    $(this).removeClass('material-icons-outlined').addClass('material-icons')
+})
+
+/* Fix the icon bar at the bottom of the page */
+$('main').css('height', innerHeight - 55)

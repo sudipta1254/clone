e2b80924c1122129d0b3d7b0cd7eb7ee4d3d1337
Sudipta Singha <singhaashoka03@gmail.com>
2024-05-05 18:46:40 +0530

    Now you can switch to email, chat or meet section & icon update

--- GoogleMeetUImobile/script.js
+++ GoogleMeetUImobile/script.js
@@ -8,16 +8,25 @@
 
 /* Change style of icons on click */
 $('.md-email').click(function () {
+    $('#div-email').show()
+    $('#div-chat').hide()
+    $('#div-meet').hide()
     $(this).removeClass('material-icons-outlined').addClass('material-icons').css('background', '#c4d8f3')
     $('.md-chat').text('chat_bubble_outline').css('background', 'transparent')
     $('.md-meet').addClass('material-icons-outlined').removeClass('material-icons').css('background', 'transparent')
 })
 $('.md-chat').click(function () {
+    $('#div-email').hide()
+    $('#div-chat').show()
+    $('#div-meet').hide()
     $('.md-email').addClass('material-icons-outlined').removeClass('material-icons').css('background', 'transparent')
     $(this).text('chat_bubble').css('background', '#c4d8f3')
     $('.md-meet').addClass('material-icons-outlined').removeClass('material-icons').css('background', 'transparent')
 })
 $('.md-meet').click(function () {
+    $('#div-email').hide()
+    $('#div-chat').hide()
+    $('#div-meet').show()
     $('.md-email').addClass('material-icons-outlined').removeClass('material-icons').css('background', 'transparent')
     $('.md-chat').text('chat_bubble_outline').css('background', 'transparent')
     $(this).removeClass('material-icons-outlined').addClass('material-icons').css('background', '#c4d8f3')
